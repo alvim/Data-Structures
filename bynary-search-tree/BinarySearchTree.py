b22fb6dabd1b8479c5ab6044df4bf658f3202b50
Mauricio Alvim <malvimmacedo@gmail.com>
2018-04-14 18:28:15 -0300

    Implementing BST.

--- bynary-search-tree/BinarySearchTree.py
+++ bynary-search-tree/BinarySearchTree.py
@@ -21,3 +21,40 @@ class Node:
 
     setRight(self, node):
         self.right = node
+
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = null
+
+    _getNext(self, node, curr_node):
+        if node.getValue() < curr_node.getValue():
+            return curr_node.getLeft()
+        else:
+            return curr_node.getRight()
+
+    getRoot(self):
+        return self.root
+
+    search(self, value):
+
+    successor(self, value):
+
+    show(self, value):
+
+    insert(self, value):
+        n = Node(value)
+        curr_node = self.root
+        next_node = self._getNext(n, curr_node)
+
+        while next_node:
+            curr_node = next_node
+            next_node = self._getNext(n, curr_node)
+        
+        # aqui next_node já é null
+        # agora, esse next_node deve ser o n
+        # como referenciar ele, sem repetir a
+        # comparação já feita dentro de getNext()?
+        # talvez se eu acessar next_node por referência...
+
+    remove(self, value):
